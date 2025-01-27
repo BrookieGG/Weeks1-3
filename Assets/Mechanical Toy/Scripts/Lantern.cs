574f4791974f6356b7d81bd109c4adfbdf1e5584
DESKTOP-SAL2D94\brook <brooke4444@me.com>
2025-01-26 22:03:50 -0500

    added some comments

--- Assets/Mechanical Toy/Scripts/Lantern.cs
+++ Assets/Mechanical Toy/Scripts/Lantern.cs
@@ -4,44 +4,44 @@ using UnityEngine;
 
 public class Lantern : MonoBehaviour
 {
-    public Vector3 startingAngle;
-    public Vector3 endingAngle;
-    private float t;
-    public float swing;
-    private bool isSwinging;
+    public Vector3 startingAngle; //stores the starting Angle number
+    public Vector3 endingAngle; //stores the final Angle number
+    private float t; //float to track time that has passed
+    public float swing; 
+    private bool isSwinging; //determines whether the lantern is swinging
     
     // Start is called before the first frame update
     void Start()
     {
         t = 0;
-        isSwinging = false;
+        isSwinging = false; //sets swinging bool to false
     }
     // Update is called once per frame
     void Update()
     {
         t += Time.deltaTime;
         {
-            if (t > swing)
+            if (t > swing) //checks if time is greater than one swing cycle
             {
-                t = 0;
+                t = 0; //reset time
                 isSwinging = !isSwinging;
             }
-            else if (isSwinging)
+            else if (isSwinging) //if the lantern is swinging call firstSwing
             {
                 firstSwing();
             }
-            else
+            else //if the lantern is not swinging call secondSwing
             {
                 secondSwing();
             }
         }
-        void firstSwing()
+        void firstSwing() //first half of the swing
         {
-            transform.eulerAngles = Vector3.Lerp(startingAngle, endingAngle, t);
+            transform.eulerAngles = Vector3.Lerp(startingAngle, endingAngle, t); //rotates the lantern using lerp based on time
         }
-        void secondSwing()
+        void secondSwing() //second half of the swing 
         {
-            transform.eulerAngles = Vector3.Lerp(endingAngle, startingAngle, t);
+            transform.eulerAngles = Vector3.Lerp(endingAngle, startingAngle, t); //rotates the lantern using lerp based on time but reverses the angles so it goes backwards
         }
     }
 }
